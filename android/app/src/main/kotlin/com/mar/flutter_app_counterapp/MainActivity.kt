5a51ec22aa25cf83e5821ca73b6f202a255c2ee6
Your Name <marwamostafaa2020@gmail.com>
2021-09-30 13:14:30 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/mar/flutter_app_counterapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mar.flutter_app_counterapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'Second.dart';
+import 'myCounter.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<MyCounter>(
+      create: (BuildContext context)=> MyCounter(),
+      child: MaterialApp(
+        home: Home(),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Consumer<MyCounter>(builder: (context, mycounter, child) {
+            return Text('${mycounter.counter}');
+          }),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.forward),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Second(),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Consumer<MyCounter>(builder: (context, mycounter, child) {
+                return Text('${mycounter.counter}');
+              }),
+              Consumer<MyCounter>(builder: (context, mycounter, child) {
+                return IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    mycounter.inc();
+                  },
+                );
+              }),
+              Consumer<MyCounter>(builder: (context, mycounter, child) {
+                return Text('${mycounter.counter}');
+              }),
+            ],
+          ),
+        ));
+  }
+}
